9911de8ea8bf25857d63cabd8beab2a6a8393bbb
Sarvesh <sarveshjaiofficial@gmail.com>
2024-01-17 12:39:35 +0530

    done with exact color theme

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 export const myNewTheme = extendTheme({
   
   config: {
-    initialColorMode: "dark",
+    initialColorMode: "system",
     useSystemColorMode: true,
   },
   colors: {
